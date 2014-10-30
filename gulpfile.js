babf35a1d536ff8d911386b599a8882f5777e6b1
Adam Weiner <adamwineguy@gmail.com>
2014-10-30 15:37:31 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+
+var paths = {
+	scripts: 'public/scripts/lib/**/*.js',
+	scriptsDest: 'publuc/scripts'
+};
+
+gulp.task('scripts', function(){
+	gulp.src(paths.scripts)
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scriptsDest))
+		.pipe(livereload({auto: false}))
+});
+
+gulp.task('watch', function () {
+	livereload.listen();
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.styles, ['styles']);
+});
